8c25b4ab8c7933c264d3cc47292ed515bf3d2bb5
Loganathan R <41115255+Logubabu@users.noreply.github.com>
2023-06-12 12:45:10 +0530

    initial commit

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+
+interface HomeProps {}
+
+const Home: FC<HomeProps> = () => (
+  <div>
+    <h1 className="text-4xl text-black font-bold">Loganathan</h1>
+    <h2 className="text-2xl text-black">Software Developer</h2>
+  </div>
+);
+
+export default Home;
